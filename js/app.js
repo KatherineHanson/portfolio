60d2ae5e00b609ee3b3be67832532d772a7e5cee
KatherineHanson <katherinemariehanson@gmail.com>
2017-07-10 23:23:35 -0700

    altered index.html text, added constructor function for project data

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 'use strict';
+
+// Project Data Constructor Function
+function Project(imageName,imageLink,projectName,projectSummary) {
+  this.imageName = imageName;
+  this.imageLink = imageLink;
+  this.projectName = projectName;
+  this.projectSummary = projectSummary;
+}
+
+var aboutMe = new Project('About Me','https://katherinehanson.github.io/about-me/','About Me Page','My first About Me page');
+
+console.log(aboutMe);
