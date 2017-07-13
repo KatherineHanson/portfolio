d6afd3c6d9239e530bcbe139f5244465f28a9c9d
KatherineHanson <katherinemariehanson@gmail.com>
2017-07-13 11:49:41 -0700

    added nav links acting as tabs functionality, navbar toggle, colors

--- js/pageView.js
+++ js/pageView.js
@@ -3,35 +3,24 @@
 // Configure a view object, to hold all our functions for dynamic updates and article-related event handlers.
 var pageView = {};
 
-pageView.populateFilters = function() {
-  $('article').each(function() {
-    var authorName, category, optionTag;
-    if (!$(this).hasClass('template')) {
-      // REVIEW: We need to take every author name from the page, and make it an option in the Author filter.
-      //       To do so, Build an `option` DOM element that we can append to the author select box.
-      //       Start by grabbing the author's name from an attribute in `this` article element,
-      //       and then use that bit of text to create the option tag (in a variable named `optionTag`),
-      //       that we can append to the #author-filter select element.
-      authorName = $(this).attr('data-author');
-      optionTag = '<option value="' + authorName + '">' + authorName + '</option>';
+pageView.handleMainNav = function() {
 
-      if ($('#author-filter option[value="' + authorName + '"]').length === 0) {
-        $('#author-filter').append(optionTag);
-      }
-
-      // REVIEW: Similar to the above, but...
-      //       Avoid duplicates! We don't want to append the category name if the select
-      //       already has this category as an option!
-      category = $(this).attr('data-category');
-      optionTag = '<option value="' + category + '">' + category + '</option>';
-      if ($('#category-filter option[value="' + category + '"]').length === 0) {
-        $('#category-filter').append(optionTag);
-      }
-    }
+  $('.navbar-header').on('click', 'button', function() {
+    $('.nav li').toggle();
   });
+
+  $('.navbar-header').on('click', '.tab', function() {
+    $('.tab-content').hide();
+    console.log($('.tab-content'));
+    $('#' + $(this).data('content')).fadeIn();
+    console.log($('#' + $(this).data('content')).html());
+  });
+
+  $('.navbar-header .tab:first').click(); // Let's now trigger a click on the first .tab element, to set up the page.
 };
 
 // TODO: Call all of the above functions, once we are sure the DOM is ready.
 $(document).ready(function() {
-  pageView.populateFilters();
+  // pageView.populateFilters();
+  pageView.handleMainNav();
 })
