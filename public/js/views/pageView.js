e0261a5290cc28dfa522525baa411123983bc132
KatherineHanson <katherinemariehanson@gmail.com>
2017-07-25 12:30:20 -0700

    IFFEs implemented, still buggy

--- public/js/views/pageView.js
+++ public/js/views/pageView.js
@@ -1,34 +1,41 @@
 'use strict';
 
-// Configure a view object, to hold all our functions for dynamic updates and article-related event handlers.
-var pageView = {};
+var app = app || {};
 
-pageView.handleMainNav = () => {
-  $('.navbar-header').on('click', 'button', () => {
-    $('li').toggleClass('li');
-  });
+(function(module) {
+  // Configure a view object, to hold all our functions for dynamic updates and article-related event handlers.
+  var pageView = {};
 
-  $('.navbar-header').on('click', '.tab', function() {
-    $('.tab-content').hide();
-    $('#' + $(this).data('content')).fadeIn();
-  });
+  pageView.handleMainNav = () => {
+    $('.navbar-header').on('click', 'button', () => {
+      $('li').toggleClass('li');
+    });
 
-  // Trigger a click on the first .tab element, to set up the page.
-  $('.navbar-header .tab:first').click();
-};
+    $('.navbar-header').on('click', '.tab', function() {
+      $('.tab-content').hide();
+      $('#' + $(this).data('content')).fadeIn();
+    });
 
-let sleepHours = [7,7,7,7,7,7,7];
-pageView.calculateSleepHours = sleepHours => {
-  let sleepTotal = sleepHours.reduce((sum, value) => {
-    return sum + value;
-  }, 0);
-  console.log('You slept ' + sleepTotal + ' hours this week!');
-}
+    // Trigger a click on the first .tab element, to set up the page.
+    $('.navbar-header .tab:first').click();
+  };
 
-// Call all of the above functions, once we are sure the DOM is ready.
-pageView.initIndexPage = () => {
-  Project.all.map(article => $('#projects').append(article.toHtml()))
+  let sleepHours = [7,7,7,7,7,7,7];
+  pageView.calculateSleepHours = sleepHours => {
+    let sleepTotal = sleepHours.reduce((sum, value) => {
+      return sum + value;
+    }, 0);
+    console.log('You slept ' + sleepTotal + ' hours this week!');
+  }
 
-  pageView.handleMainNav();
-  pageView.calculateSleepHours(sleepHours);
-};
+  // Call all of the above functions, once we are sure the DOM is ready.
+  pageView.initIndexPage = () => {
+    Project.all.map(article => $('#projects').append(article.toHtml()))
+
+    pageView.handleMainNav();
+    pageView.calculateSleepHours(sleepHours);
+  };
+
+  Project.fetchAll(pageView.initIndexPage);
+  module.pageView = pageView;
+})(window);
