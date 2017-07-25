d7117cb2d0d7ce286d5474dfcfe0ca836ae95e6f
KatherineHanson <katherinemariehanson@gmail.com>
2017-07-25 11:55:08 -0700

    finished all functionality except IFFEs

--- public/js/models/project.js
+++ public/js/models/project.js
@@ -22,33 +22,21 @@ Project.loadAll = rows => {
 
 // Function that retrieves the data from either a local or remote
 // source, and processes it, then hands off control to the View.
-
-
-Project.fetchAll = function() {
+Project.fetchAll = () => {
   if (localStorage.rawData) {
-    // When rawData is already in localStorage,
-    // load it with the .loadAll function above,
-    // and then render the index page (using the proper method on the articleView object).
-    Project.loadAll($.parseJSON(localStorage.rawData)); //DONE: What do we pass in to loadAll()?
-     //Method to render the index page
+    Project.loadAll($.parseJSON(localStorage.rawData));
     pageView.initIndexPage();
   } else {
-    // When rawData isn't already in localStorage,
-    // retrieve the JSON file from the server with AJAX,
-    // cache it in localStorage,
-    // load all the data into Project.all with the .loadAll function above,
-    // render the index page.
     $.getJSON('data/codeProjects.json')
        .then(
-       //stringify into localstorage
-       function(data) {
-         console.log(data)
-         localStorage.setItem('rawData', JSON.stringify(data))
+       data => {
+         console.log(data);
+         localStorage.setItem('rawData', JSON.stringify(data));
          Project.loadAll($.parseJSON(localStorage.rawData));
          pageView.initIndexPage();
        },
-       function(err) {
-         console.log(err)
+       err => {
+         console.log(err);
        })
   }
 }
