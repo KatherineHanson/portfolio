7daa8ffd00fd76eddad7370382a4faf7dc31d14c
KatherineHanson <katherinemariehanson@gmail.com>
2017-07-27 11:26:18 -0700

    added middleware for request and request route

--- public/js/model/repo.js
+++ public/js/model/repo.js
@@ -7,25 +7,9 @@ var app = app || {};
   repos.all = [];
 
   repos.requestRepos = function(callback) {
-    $.ajax({
-      url: 'https://api.github.com/user/repos?type=owner',
-      method: 'GET',
-      headers: {
-        Authorization: `token ${GITHUB_TOKEN}`
-      }
-    })
-      .then(
-       data => {
-         console.log(data);
-         repos.all = data;
-       },
-       err => {
-         console.error(err);
-       }
-      )
-      .then(
-        callback
-      )
+    $.get('github/user/repos')
+    .then(data => repos.all = data, err => console.error(err)) // es6 syntax arrow functions
+    .then(callback);
   };
 
   // Model method that filters the full collection for repos with a particular attribute.
